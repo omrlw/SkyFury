e120c276bec94416b3662e6006399b99b4c4330a
omrlw <143002070+omrlw@users.noreply.github.com>
2023-10-06 02:53:49 -0500

    Agregado .gitignore para excluir directorios y archivos generados

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    // Load a sprite to display
+    sf::Texture texture;
+
+    sf::Sprite sprite(texture);
+
+    // Create a graphical text to display
+    sf::Font font;
+
+    sf::Text text("Hello SFML", font, 50);
+
+    // Load a music to play
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear screen
+        window.clear();
+
+        // Draw the sprite
+        window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
