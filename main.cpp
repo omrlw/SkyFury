064d0fddc251fe8a1eeef21397f92f50241e543e
Sebastian Chacon <143002070+omrlw@users.noreply.github.com>
2023-10-19 16:35:58 -0500

    test branch

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include "avion.h"
-#include "bala.h"
-#include "enemigo.h"
 
 //Propiedades de la ventana
 const int ANCHO_VENTANA = 800;
@@ -16,10 +13,6 @@ int main()
     // Limitamos la tasa de actualiazcion de la ventana
     window.setFramerateLimit(60);
 
-
-    Avion avion(100, 100, 3, 3,5);
-    Bala bala(100,110,1,10);
-
     // Bucle principal del juego
     while (window.isOpen()) {
         sf::Event event{}; //Inicializar el objetos tipo Evento
@@ -30,9 +23,7 @@ int main()
             }
 
         window.clear(); //Limpiar la ventana para borrar el contenido anterior.
-        avion.pintar_avion(window); //Representar los objetos del juego en la ventana
-        avion.mover_avion(window); //Actualizar la lógica del juego
-        bala.mover_bala(window, avion);
+
         window.display(); //Mostrar la ventana para que el jugador pueda ver el resultado.
 
 
